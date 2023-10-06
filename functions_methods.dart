2f2e283890f658c2bba1b85500aa37306e3aed52
therasuldev <rasul.ramixanov@gmail.com>
2023-10-06 13:55:07 +0400

    functions

---
+++ functions_methods.dart
@@ -0,0 +1,25 @@
+//! Funksiya ile metodun ferqi : funksiya geriye data type qaytarmir.
+void main() {
+  final func = myFunc1();
+  print(func);
+
+  userParameters(name: "Flutter", lastName: "Dart", age: 50);
+}
+
+void myFunc() {
+  print('funksiya');
+  return; // bos,return prosesi dayandirmaq ucun istifade olunur;
+}
+
+String myFunc1() {
+  return "45";
+}
+
+void userParameters({required String name, required String lastName, required int age}) {
+  // required - parametrinin adını mutleq yazmaliyiq (type-in qabaginda sual isaresi yoxdursa)
+  print("name: $name = lastName: $lastName");
+}
+
+void printAge(int age) {
+  print(age);
+}
